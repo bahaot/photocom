059e200c90b3e0720dcd1273674b5268f3bbcccb
bahaot <bahaalout1@gmail.com>
2022-11-04 17:14:25 +0200

    filling data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+
 import "./App.css";
 
 import DATA from "./Data/Data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// the coming data is look like this
+/*
+[
+  id: "text",
+  firstName: "text",
+  lastName: "text",
+  email: "text",
+  stories: ["array of stroies"],
+  timeLine: ["array of posts"],
+  peopleToFollow: ["array of random users"],
+] 
+*/
+
+const App = () => {
+  const [users, setUser] = useState(DATA);
+  // since this is not a real application we are going to work with only one user
+  return <div className="App"></div>;
+};
 
 export default App;
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,8 +1,132 @@
-const DATA = {
-  user: {
+const DATA = [
+  {
+    id: "userka107",
     firstName: "test",
     lastName: "test",
+    email: "userka107@example.com",
+
+    stories: [
+      {
+        id: "1YQ",
+        firstName: "Chiemeka",
+        lastName: "",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/4.jpg",
+      },
+      {
+        id: "5YS",
+        firstName: "Sanni",
+        lastName: "Hiltunen",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/30.jpg",
+      },
+      {
+        id: "18W",
+        firstName: "Malin",
+        lastName: "Green",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/38.jpg",
+      },
+      {
+        id: "WNE",
+        firstName: "Leah",
+        lastName: "Turner",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/72.jpg",
+      },
+      {
+        id: "XCL",
+        firstName: "Chiemeka",
+        lastName: "",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/4.jpg",
+      },
+      {
+        id: "VIU",
+        firstName: "Todd",
+        lastName: "Rice",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/52.jpg",
+      },
+      {
+        id: "KN5",
+        firstName: "Marischa",
+        lastName: "Cordes",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/88.jpg",
+      },
+    ],
+
+    timeLine: {
+      following: [
+        {
+          id: "FU1",
+          firstName: "Abigail",
+          lastName: "Diesen",
+          profileImg: "https://randomuser.me/api/portraits/thumb/women/11.jpg",
+          coverImg:
+            "https://images.unsplash.com/photo-1667487108463-88064c14b8dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1938&q=80",
+        },
+        {
+          id: "FU2",
+          firstName: "Trupti",
+          lastName: "Chiplunkar",
+          profileImg: "https://randomuser.me/api/portraits/thumb/women/85.jpg",
+          coverImg:
+            "https://images.unsplash.com/photo-1667481020478-8ebcbcdd9869?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        },
+        {
+          id: "FU3",
+          firstName: "Abigail",
+          lastName: "Diesen",
+          profileImg: "https://randomuser.me/api/portraits/thumb/women/11.jpg",
+          coverImg:
+            "https://images.unsplash.com/photo-1667481020991-31186b791c13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        },
+        {
+          id: "FU4",
+          firstName: "Enno",
+          lastName: "Sacher",
+          profileImg: "https://randomuser.me/api/portraits/thumb/men/72.jpg",
+          coverImg:
+            "https://images.unsplash.com/photo-1667471728450-c3a75d76ff82?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
+        },
+        {
+          id: "FU5",
+          firstName: "Dale",
+          lastName: "Edwards",
+          profileImg: "https://randomuser.me/api/portraits/thumb/men/80.jpg",
+          coverImg:
+            "https://images.unsplash.com/photo-1667420137384-8e7c17369e0e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        },
+        {
+          id: "FU6",
+          firstName: "Louise",
+          lastName: "Moreau",
+          profileImg: "https://randomuser.me/api/portraits/thumb/women/29.jpg",
+          coverImg:
+            "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        },
+      ],
+    },
+
+    peopleToFollow: [
+      {
+        id: "PTF1",
+        username: "@Vladimir",
+        firstName: "Vladimir",
+        lastName: "Jevtić",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/31.jpg",
+      },
+      {
+        id: "PTF2",
+        username: "@Koppert2020",
+        firstName: "Hinderkien",
+        lastName: "Koppert",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/61.jpg",
+      },
+      {
+        id: "PTF3",
+        username: "",
+        firstName: "Philip",
+        lastName: "Ennis",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/43.jpg",
+      },
+    ],
   },
-};
+];
 
 export default DATA;
